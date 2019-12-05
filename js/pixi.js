3b8e86b0d43cc2c1e56f30ebcfe47c40e4841476
ahcleung <31660669+ahcleung@users.noreply.github.com>
2019-12-05 18:18:09 -0500

    Initial PWA upload

---
+++ js/pixi.js
@@ -0,0 +1,136 @@
+let Application = PIXI.Application,
+	loader = PIXI.loader,
+	resources = PIXI.loader.resources,
+	Sprite = PIXI.Sprite,
+	Text = PIXI.Text;
+
+const app = new Application({
+	autoResize: true, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
+});
+document.body.appendChild(app.view);
+
+loader
+	.add([
+		"img/monkey3.png",
+		"img/monkey2.png"
+	])
+	.on("progress", loadProgressHandler)
+	.load(setup);
+
+function loadProgressHandler(loader, resource) {
+
+  //Display the file `url` currently being loaded
+  console.log("loading: " + resource.url); 
+
+  //Display the percentage of files currently loaded
+  console.log("progress: " + loader.progress + "%"); 
+
+  //If you gave your files names as the first argument 
+  //of the `add` method, you can access them like this
+  //console.log("loading: " + resource.name);
+}
+
+let state, stats, consoleScreen;
+const container = new PIXI.Container();
+
+function setup(){	
+	app.stage.addChild(container);
+	// Create a new texture
+	const texture = PIXI.Texture.from('img/monkey3.png');
+
+	consolePrint("SETUP");
+	// PIXI.settings.ROUND_PIXELS = true;
+
+	// Create a 5x5 grid of bunnies
+	for (let i = 0; i < 25; i++) {
+		const bunny = new Sprite(texture);
+		bunny.anchor.set(0.5);
+		bunny.x = (i % 5) * 40;
+		bunny.y = Math.floor(i / 5) * 40;
+		container.addChild(bunny);
+	}
+
+	// Move container to the center
+	container.x = app.renderer.width / 2;
+	container.y = app.renderer.height / 2;
+
+	// Center bunny sprite in local container coordinates
+	container.pivot.x = container.width / 2;
+	container.pivot.y = container.height / 2;
+
+	stats = new Text("Resolution: " + app.renderer.resolution +
+		"\nInner Width: " + window.innerWidth + 
+		"\nInner Height: " + window.innerHeight);
+
+	app.stage.addChild(stats);
+
+	consoleScreen = new Text("Console: ");
+	app.stage.addChild(consoleScreen);
+	consoleScreen.x = 300;
+
+	consoleScreen.text += "\nSetup";
+
+	window.addEventListener('resize', resize);
+
+	resize();
+
+	state = play;
+	app.ticker.add(delta => gameLoop(delta));
+}
+
+function gameLoop(delta){
+	state(delta);
+}
+
+function play(delta){
+	container.rotation -= 0.01 * delta;
+	stats.text = "Resolution: " + app.renderer.resolution +
+		"\nInner Width: " + window.innerWidth + 
+		"\nInner Height: " + window.innerHeight +
+		"\nContainer: " + container.x + ", " + container.y + ", " + container.width + ", " + container.height;
+}
+
+// // Listen for animate update
+// app.ticker.add((delta) => {
+// 	// rotate the container!
+// 	// use delta to create frame-independent transform
+// 	container.rotation -= 0.01 * delta;
+// });
+
+// Listen for window resize events
+
+
+// Resize function window
+function resize() {
+	// Resize the renderer
+	console.log(
+		"PRE-RESIZE" + 
+		"\nResolution: " + app.renderer.resolution +
+		"\nInner Width: " + window.innerWidth + 
+		"\nInner Height: " + window.innerHeight +
+		"\nContainer: " + container.x + ", " + container.y + ", " + container.width + ", " + container.height
+		);
+
+	app.renderer.resize(window.innerWidth, window.innerHeight);
+	container.x = app.renderer.width / 2 / window.devicePixelRatio;
+	container.y = app.renderer.height / 2 / window.devicePixelRatio;
+	//app.renderer.resolution = window.devicePixelRatio;
+	consolePrint("RESIZE");
+
+	consoleScreen.text += "\nRESIZE";
+	// You can use the 'screen' property as the renderer visible
+	// area, this is more useful than view.width/height because
+	// it handles resolution
+}
+
+
+
+function consolePrint(fromText){
+	console.log(
+		fromText + 
+		"\nResolution: " + app.renderer.resolution +
+		"\nWidth: " + app.renderer.width + 
+		"\nHeight: " + app.renderer.height +
+		"\nContainer: " + container.x + ", " + container.y + ", " + container.width + ", " + container.height
+		);
+}
---
+++ sw.js
@@ -0,0 +1,63 @@
+var cacheName = 'phaser-v1';
+var filesToCache = [
+	'./',
+  	'./index.html',
+	'./sw.js',
+	'./manifest.json',
+	'./img/logo.png',
+	'./img/icon-192.png',
+	'./img/icon-256.png',
+	'./img/icon-512.png',
+	'./img/bg2.jpg',
+	'./img/leper.ability.five.png',
+  	'./img/ability_move.png',
+  	'./img/monkey2.png',
+  	'./img/monkey3.png',
+	'./js/game.js',
+	'./js/pixi.js',
+	'./css/style.css',
+	'//cdn.jsdelivr.net/npm/phaser@3.20.0/dist/phaser.min.js',
+	'https://cdnjs.cloudflare.com/ajax/libs/pixi.js/5.1.3/pixi.min.js', 
+	'https://www.gstatic.com/firebasejs/7.1.0/firebase-app.js',
+	'https://www.gstatic.com/firebasejs/7.1.0/firebase-analytics.js',
+	'https://www.gstatic.com/firebasejs/7.1.0/firebase-database.js',
+  	'https://www.gstatic.com/firebasejs/7.1.0/firebase-firestore.js'
+];
+
+self.addEventListener('install', function(event) {
+	console.log('sw install');
+	event.waitUntil(
+		caches.open(cacheName).then(function(cache) {
+			console.log('sw caching files');
+			return cache.addAll(filesToCache);
+		}).catch(function(err) {
+			console.log(err);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	console.log('sw fetch');
+	console.log(event.request.url);
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response || fetch(event.request);
+		}).catch(function (error) {
+			console.log(error);
+		})
+	);
+});
+
+self.addEventListener('activate', function(event) {
+	console.log('sw activate');
+	event.waitUntil(
+		caches.keys().then(function(keyList) {
+			return Promise.all(keyList.map(function(key) {
+				if (key !== cacheName) {
+					console.log('sw removing old cache', key);
+					return caches.delete(key);
+				}
+			}));
+		})
+	);
+});
