dddd28ad9b1fa2f234ae32634e2e34282efc903f
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-02-05 13:03:25 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -248,7 +248,7 @@ function setup(){
 	enemy3 = new PIXI.AnimatedSprite(framesIdleMamoswine);
 	enemy3.x = 1*50*spriteSpacer;
 	enemy3.y = 0;
-	enemy3.scale.set(-globalScale*1.25, globalScale*1.25);
+	enemy3.scale.set(-globalScale*1.33, globalScale*1.33);
 	enemy3.anchor.set(anchorX2,anchorY2)
 	enemy3.animationSpeed = 0.5;
 	enemy3.play();	
