3d3bb3510e21e03b2d4b971ae6d63b354f0a8a56
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-02-06 21:09:43 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -115,7 +115,7 @@ function setup(){
 	
 // 	const obj = JSON.parse(id);
 	
-	debug = new Text("Monster name: " + obj.monsters[0].name);
+	debug = new Text("Monster name: " + obj.monsters.name);
 	debug.x = 200;
 	debug.y = 400;
 	
