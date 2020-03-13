965f970422a3f4d540196218c20d8c90f7c33a38
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-03-12 22:16:57 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -122,17 +122,17 @@ var textureButton, textureButtonDown;
 var db = firebase.firestore();
 
 //Write to firestore
-db.collection("users").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-});
+// db.collection("users").add({
+//     first: "Ada",
+//     last: "Lovelace",
+//     born: 1815
+// })
+// .then(function(docRef) {
+//     console.log("Document written with ID: ", docRef.id);
+// })
+// .catch(function(error) {
+//     console.error("Error adding document: ", error);
+// });
 
 //Read from firestore
 db.collection("users").get().then((querySnapshot) => {
