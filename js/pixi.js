aa6385babd415d517447c95db17b476bcbd98532
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-03-12 22:11:52 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -121,6 +121,18 @@ var textureButton, textureButtonDown;
 
 var db = firebase.firestore();
 
+db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+
 //var isFullScreen = false;
 
 // const framesIdleFlygon = [];
