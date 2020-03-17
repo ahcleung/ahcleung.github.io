d4cc3621c3ea511e6dad0effff5ad5b7961e68fb
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-03-17 14:33:04 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -30,7 +30,10 @@ loader
 		{name:'goat2_2_texture_png', url:'img/goat2_2_tex.png'},
 		{name:'hel1_skeleton', url:'img/hel1_ske.json'},
 		{name:'hel1_texture_json', url:'img/hel1_tex.json'},
-		{name:'hel1_texture_png', url:'img/hel1_tex.png'}
+		{name:'hel1_texture_png', url:'img/hel1_tex.png'},
+		{name:'hel2_skeleton', url:'img/hel2_ske.json'},
+		{name:'hel2_texture_json', url:'img/hel2_tex.json'},
+		{name:'hel2_texture_png', url:'img/hel2_tex.png'}
 	])
 	.on("progress", loadProgressHandler)
 	.load(setup);
