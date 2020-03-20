1092a885dc9f3582d1e2f94518954f7aaef19428
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-03-19 21:47:47 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -553,7 +553,8 @@ function createSprite(direction, item, index){
 	healthBar.addChild(innerBar);
 	healthBar.inner = innerBar;	
 	
-	let textHP = new Text(item.statCalc[0] + "/" + item.EHP, {fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
+	let textHP = new Text(item.statCalc[0] + "/" + item.EHP, {fontFamily : 'Arial', fontSize: 24, fill : 0xffffff, align : 'center'});
+	textHP.anchor(0.5, 0.5);
 	textHP.x = 94.5;
 	textHP.y = 20;
 	healthBar.addChild(textHP);
