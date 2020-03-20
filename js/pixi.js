241a738e7399895764ad6808bbbe3bee695e55be
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-03-20 11:55:55 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -5,7 +5,7 @@ let Application = PIXI.Application,
 	Text = PIXI.Text;
 
 const app = new Application({
-	autoResize: true, transparent: true, resolution: devicePixelRatio || 1,
+	autoResize: true, transparent: true, resolution: devicePixelRatio || 2,
 });
 document.querySelector('#frame').appendChild(app.view);
 
