e05bf3c39ed7fe8f4f1771df9cfa9cbeffac6195
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-03-30 14:29:07 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -402,9 +402,9 @@ function setup(){
 				let posMarker = new PIXI.Graphics();
 				if(j == 0){
 					if(movesList.data.moves[arrayHero[1].moves[i]].position[k] == 1){
-						posMarker.beginFill(0x66cc66).drawRect(0, -14, 14, 14);
+						posMarker.beginFill(0x66cc66).drawRect(0, -13, 13, 13);
 					}else{
-						posMarker.beginFill(0x636363).drawRect(0, -14, 14, 14);
+						posMarker.beginFill(0x636363).drawRect(0, -13, 13, 13);
 					}
 					posMarker.x = 25 * (j+k);
 				}else{
