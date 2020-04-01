bf3f836a6291a1f06f3bf923aed63faf287bdd88
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-01 15:25:55 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -186,7 +186,7 @@ const hpHeroContainerArray = [];	//Array of hero HP containers
 const hpEnemyContainerArray = [];	//Array of enemy HP containers
 const moveArray = [];			//Array of move containers
 
-const elementEarth = PIXI.Texture.from(resources['element_earth'].texture);
+const elementEarth = PIXI.Texture.from('img/element_earth.png');
 
 const vita = [];
 vita[0] = {
