6b5c71301cb2c0630d44bb59d392ce12d81b2af8
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-06 10:50:58 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -646,8 +646,8 @@ function play(delta){
 		"\nInner Height: " + window.innerHeight +
 		"\nAppScreen Width: " + app.screen.width + 
 		"\nAppScreen Height: " + app.screen.height +
-		"\nScale: " + (Math.cos(phase) + 1) * 1.2;	
-	hpHeroContainerArray[0].select.scale.x = (Math.cos(phase) + 1) * 1.2;
+		"\nScale: " + (Math.cos(phase) + 1) * 0.5 + 1;	
+	hpHeroContainerArray[0].select.scale.x = (Math.cos(phase) + 1) * 0.5 + 1;
 }
 
 // Listen for animate update
