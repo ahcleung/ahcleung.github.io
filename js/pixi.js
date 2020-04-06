3235daa0cdf6be8d41b8573b0974fa478c11ce39
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-06 11:00:35 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -640,14 +640,14 @@ function gameLoop(delta){
 
 let phase = 0.0;
 function play(delta){
-	phase += delta / 30.0;
+	phase += delta / 20.0;
 	onScreenStats.text = "ResolutionTest5: " + app.renderer.resolution +
 		"\nInner Width: " + window.innerWidth + 
 		"\nInner Height: " + window.innerHeight +
 		"\nAppScreen Width: " + app.screen.width + 
 		"\nAppScreen Height: " + app.screen.height +
-		"\nScale: " + (Math.cos(phase) + 1) * 0.05 + 1;	
-	hpHeroContainerArray[0].select.scale.x = (Math.cos(phase) + 1) * 0.05 + 1;
+		"\nScale: " + (Math.cos(phase) + 1) * 0.04 + 1;	
+	hpHeroContainerArray[0].select.scale.x = (Math.cos(phase) + 1) * 0.04 + 1;
 }
 
 // Listen for animate update
