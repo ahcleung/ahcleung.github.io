67006c3f7ccc1306268d84b1e707151ee6d1e169
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-07 13:41:21 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1503,7 +1503,7 @@ function onCreatureDown(){
 		
 		console.log(index + ": " + movesList.data.moves[element].tags);
 		movesList.data.moves[element].tags.forEach(tagName =>{
-			console.log(movesList.data.moves[element].[tagName]);
+			console.log(movesList.data.moves[element][14]);
 		});
 // 		console.log(index + " Position: " + movesList.data.moves[element].position + " |Target: " + movesList.data.moves[element].target);
 	});	
