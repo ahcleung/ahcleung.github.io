d60c1fa7890319a49a38555e0279e91117e241e8
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-07 18:49:13 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1723,7 +1723,7 @@ function onMoveDown(){
 }
 
 function onAdditionalDown(){
-	moveArray[0].targetText.style = {fill : 0x66cc66};	
+	moveArray[0].targetText.style.fill = {fill : 0x66cc66};	
 	
 	onScreenStats.visible = true;
 	consoleScreen.visible = true;
