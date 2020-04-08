9edaf071703efcd5cb75b4f4533c189787b116df
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-07 23:02:38 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -530,7 +530,7 @@ function setup(){
 		
 		for (var j = 0; j < 3; j++){
 			let posMarker = new PIXI.Graphics();				
-			posMarker.beginFill(0xffffff).drawRect(0, -4, 20, 6);
+			posMarker.beginFill(0xFF6961).drawRect(0, -4, 20, 6);
 			posMarker.x = 25 * j;
 			markerTargetEnemySeveralArray.push(posMarker);
 			markerTargetEnemySeveralContainer.addChild(posMarker);
