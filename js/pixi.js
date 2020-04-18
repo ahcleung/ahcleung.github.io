e11fc23106e937d321e1a794c2f265207e5731dd
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-18 00:29:16 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -1960,9 +1960,9 @@ function onExtrasDown(){
 	arrayHero.forEach((arrayCreature,arrayCreatureIndex) => {
 		var calcSpeed;
 		if(arrayCreature.statMod[6]>0){
-			calcSpeed = (arrayCreature.speed/5)*((Math.abs(arrayCreature.statMod[6])+2)/2)+(Math.floor(Math.random() * 20) + 1);
+			calcSpeed = (arrayCreature.speed/4)*((Math.abs(arrayCreature.statMod[6])+2)/2)+(Math.floor(Math.random() * 20) + 1);
 		}else{
-			calcSpeed = (arrayCreature.speed/5)*(2/(Math.abs(arrayCreature.statMod[6])+2))+(Math.floor(Math.random() * 20) + 1);
+			calcSpeed = (arrayCreature.speed/4)*(2/(Math.abs(arrayCreature.statMod[6])+2))+(Math.floor(Math.random() * 20) + 1);
 		}
 		console.log(arrayCreatureIndex + " Pre-Speed: " + arrayCreature.speed + "| CalcSpeed: " + calcSpeed);
 		speedTracker.push(calcSpeed);
@@ -1970,9 +1970,9 @@ function onExtrasDown(){
 	arrayEnemy.forEach((arrayCreature,arrayCreatureIndex) => {
 		var calcSpeed;
 		if(arrayCreature.statMod[6]>0){
-			calcSpeed = (arrayCreature.speed/5)*((Math.abs(arrayCreature.statMod[6])+2)/2)+(Math.floor(Math.random() * 20) + 1);
+			calcSpeed = (arrayCreature.speed/4)*((Math.abs(arrayCreature.statMod[6])+2)/2)+(Math.floor(Math.random() * 20) + 1);
 		}else{
-			calcSpeed = (arrayCreature.speed/5)*(2/(Math.abs(arrayCreature.statMod[6])+2))+(Math.floor(Math.random() * 20) + 1);
+			calcSpeed = (arrayCreature.speed/4)*(2/(Math.abs(arrayCreature.statMod[6])+2))+(Math.floor(Math.random() * 20) + 1);
 		}
 		console.log(arrayCreatureIndex + " Pre-Speed: " + arrayCreature.speed + "| CalcSpeed: " + calcSpeed);
 		speedTracker.push(calcSpeed);
