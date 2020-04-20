cabf0453945f6d845882431131be0c0ebad08038
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-20 15:21:20 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -216,15 +216,16 @@ hero[0] = {
 	statDODG: 20, statHP: 35, statPATK: 10, statPDEF: 50, statSATK: 0, statSDEF: 0, statSPD: 40
 };
 hero[1] = {
-	id: 2, level: 45, 
-	skill1: 4, skill2: 10, skill3: 11, skill4: 1,
-	statDODG: 20, statHP: 35, statPATK: 40, statPDEF: 10, statSATK: 0, statSDEF: 3, statSPD: 47
-};
-hero[2] = {
 	id: 10, level: 47, 
 	skill1: 4, skill2: 0, skill3: 6, skill4: 1,
 	statDODG: 20, statHP: 35, statPATK: 0, statPDEF: 3, statSATK: 40, statSDEF: 20, statSPD: 39
 };
+hero[2] = {
+	id: 2, level: 45, 
+	skill1: 4, skill2: 10, skill3: 11, skill4: 1,
+	statDODG: 20, statHP: 35, statPATK: 40, statPDEF: 10, statSATK: 0, statSDEF: 3, statSPD: 47
+};
+
 // hero[3] = {
 // 	id: 9, level: 47, 
 // 	skill1: 4, skill2: 0, skill3: 6, skill4: 1,
