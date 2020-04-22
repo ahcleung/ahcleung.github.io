5a32ccb90ae14ce36d496eade079ba6d4909b22f
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-22 14:58:55 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -1658,14 +1658,13 @@ function onCreatureDown(){
 					console.log("Attack: " + attack);
 					console.log("Defense: " + defense);
 					console.log("Power: " + skillsList.data.skills[selectedSkill].power);
-					damage = (((((2*level)/5) + 2 * skillsList.data.skills[selectedSkill].power * (attack/defense))/150) + 2) * (Math.random() * (1 - 0.85) + 0.85);
+					damage = ((((((2*level/5) + 2) * skillsList.data.skills[selectedSkill].power * (attack/defense))/150) + 2);
+					// damage = (((((2*level)/5)+2*skillsList.data.skills[selectedSkill].power*(attack/defense))/150)+2);
 					console.log(targeted + " takes " + damage + " damage");
 				});
 			}else{
 				console.log(validPositionTargetArray[clickedTarget] + " takes ## damage");
 			}
-
-
 
 			var identifier = [];
 			if(turnArray[0] > 0){
