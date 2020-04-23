d8248921cb2fdae63862adb99c487ae7a9b542fa
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-23 14:04:56 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -348,7 +348,7 @@ function setup(){
 	// app.stage.addChild(rectTemp);
 
 	const style = new PIXI.TextStyle({
-        fontFamily: 'Arial',
+        fontFamily: styleFontFamily,
         fontSize: 36,
 //         fontStyle: 'italic',
         fontWeight: 'bold',
