2cb024466c2ac5b28c7ebf571fb0b300c323bf1b
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-23 18:00:36 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -356,7 +356,7 @@ function setup(){
 	tempContainer.addChild(damageText);
 	tempContainer.damageText = damageText;
 
-	tween = TweenMax.to(damageText, 1, {y: -100, alpha: 0, paused: true});
+	tween = TweenMax.to(damageText, 1, {ease: SlowMo.ease.config(0.7, 0.7, false), y: -100, alpha: 0, paused: true});
 	tween.duration(10);
 
 	app.stage.addChild(tempContainer);
