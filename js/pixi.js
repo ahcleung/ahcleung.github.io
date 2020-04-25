ac172e6a963bcbb5ebc173f2494bdfe03cebf120
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-24 22:58:46 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -967,13 +967,22 @@ function createSprite(direction, item, index){
 	// dmgCounter.x = 100;
 	// dmgCounter.y = 500;
 
-	var tween2 = gsap.timeline({paused: true});
-	tween2
-		.to(dmgCounter, { duration: 0.5, ease:"expo.in", alpha: 1})
-		.to(dmgCounter.scale, { duration: 0.5, ease:"expo.in", x: 2, y: 2}, 0);
-	tween2
-		.to(dmgCounter, { duration: 1.25, ease:"expo.inOut", y: -300, alpha: 0})
-		.to(dmgCounter.scale, { duration: 1.25, ease:"expo.inOut", x: 1, y: 1}, 0.1);
+	// var tween2 = gsap.timeline({paused: true});
+	// tween2
+	// 	.to(dmgCounter, { duration: 0.5, ease:"expo.in", alpha: 1})
+	// 	.to(dmgCounter.scale, { duration: 0.5, ease:"expo.in", x: 2, y: 2}, 0);
+	// tween2
+	// 	.to(dmgCounter, { duration: 1.25, ease:"expo.inOut", y: -300, alpha: 0})
+	// 	.to(dmgCounter.scale, { duration: 1.25, ease:"expo.inOut", x: 1, y: 1}, 0.1);
+
+	var tween2 = new TimelineMax({paused: true});
+	tween2.to(dmgCounter2, 0.5, {ease:Expo.easeIn, alpha: 1});
+	tween2.to(dmgCounter2.scale, 0.5, {ease:Expo.easeIn, x: 2, y: 2},0);
+	tween2.to(dmgCounter2, 1.25, {ease:Expo.easeInOut, y: -300, alpha: 0})
+	tween2.to(dmgCounter2.scale, 1.25, {ease:Expo.easeInOut, x: 1, y: 1},0.5);
+
+
+
 	dmgCounter.alpha = 0;
 
 	dmgCounter.tween = tween2;
