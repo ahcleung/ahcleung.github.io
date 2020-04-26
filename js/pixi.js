86a51e05325845ee14b2ecda1bd8b95156c0c497
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-26 18:24:28 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -1079,7 +1079,7 @@ function createSprite(direction, item, index){
 	
 	let critDmgBar = new PIXI.Graphics();
 	critDmgBar.beginFill(0xff7b00);
-	critDmgBar.drawRect(0, 0, 0, 40);
+	critDmgBar.drawRect(0, 0, 1, 40);
 	critDmgBar.endFill();
 	dmgContainer.addChild(critDmgBar);
 	dmgContainer.critDmgBar = critDmgBar;
