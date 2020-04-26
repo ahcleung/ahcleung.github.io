6c89fd62a193082c6f9200df1521749927194c16
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-26 15:13:28 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -1012,12 +1012,12 @@ function createSprite(direction, item, index){
 	// 	dmgNum.visible = false;
 	// }});
 	tween2.to(dmgPopup, 0.2, {ease:Expo.easeIn, alpha: 1});
-	tween2.fromTo(dmgNum.scale, 0.2, {x: 0.5, y: 0.5}, {ease:Expo.easeIn, x: 1, y: 1}, 0);
+	tween2.fromTo(dmgPopup.scale, 0.2, {x: 0.5, y: 0.5}, {ease:Expo.easeIn, x: 1, y: 1}, 0);
 	// tween2.to(dmgPopup.scale, 0.2, {ease:Expo.easeIn, x: 1, y: 1}, 0);
 	tween2.to(dmgPopup, 1.5, {delay: 0.5, ease:Expo.easeInOut, y: -300, alpha: 0})
 	tween2.to(dmgPopup.scale, 1.5, {delay: 0.5, ease:Expo.easeInOut, x: 0.5, y: 0.5}, 0.2);
 
-	// dmgNum.alpha = 0;
+	dmgPopup.alpha = 0;
 
 	dmgNum.tween = tween2;
 	
