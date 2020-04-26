15d94da39aad9e06dd399a4d55b2dd6ce317b82e
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-04-26 00:17:13 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -960,7 +960,7 @@ function createSprite(direction, item, index){
 	const dmgText = new PIXI.Container();
 
 	const style = new PIXI.TextStyle({
-        fontFamily: 'Bevan',
+        fontFamily: 'Nova Round',
         fontSize: 45,
         // fontStyle: 'italic',
         fontWeight: 700,
