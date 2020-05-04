5932435adb61ba3b9615afb1b32178f982c319ac
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-05-04 19:00:16 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -846,9 +846,9 @@ function setup(){
 	}});
 
 	var actionTween2 = new TimelineMax({paused: true});
-	actionTween2.to(attackSpriteBack, 1, {ease:Expo.easeOut, x: 350});
+	actionTween2.to(attackSpriteBack, 1, {ease:Expo.easeOut, x: 250});
 	actionTween2.to(attackSpriteAttack, 1, {ease:Expo.easeOut, x: 300},0);
-	actionTween2.to(attackSpriteTop, 1, {ease:Expo.easeOut, x: 350},0);
+	actionTween2.to(attackSpriteTop, 1, {ease:Expo.easeOut, x: 250},0);
 
 	// var dmgPopupTween = new TimelineMax({paused: true});
 	// dmgPopupTween.to(dmgPopup, 0.2, {ease:Expo.easeIn, alpha: 1});
