14ce8b5582f6e417192301aa44acaf2a94a9361e
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-05-05 11:05:21 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -842,7 +842,7 @@ function setup(){
 	defendSpriteMiss.anchor.set(0,1);
 
 	defendSpriteReady.x = 200;
-	defendSpriteReady.y = -10;
+	defendSpriteReady.y = 10;
 	defendSpriteMiss.x = 100;
 
 	attackSpriteReady.scale.set(0.55);
