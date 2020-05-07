3bae6f672eff4ad5d27f7db348367bea23b27b6f
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-05-07 18:01:37 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -1172,19 +1172,19 @@ function createSprite(direction, item, index){
 
 	var sprite_p_fxBack = new PIXI.Sprite(resources[item.code + '_p_fxBack'].texture);
 	sprite_p_fxBack.anchor.set(1);
-	sprite_p_fxBack.visible = false;
+	// sprite_p_fxBack.visible = false;
 	// actionArray.push(sprite_p_fxBack);
 	creatureAction.addChild(sprite_p_fxBack);
 
 	var sprite_p_attack = new PIXI.Sprite(resources[item.code + '_p_attack'].texture);
 	sprite_p_attack.anchor.set(1);
-	sprite_p_attack.visible = false;
+	// sprite_p_attack.visible = false;
 	// actionArray.push(sprite_p_attack);
 	creatureAction.addChild(sprite_p_attack);
 
 	var sprite_p_fxTop = new PIXI.Sprite(resources[item.code + '_p_fxTop'].texture);
 	sprite_p_fxTop.anchor.set(1);
-	sprite_p_fxTop.visible = false;
+	// sprite_p_fxTop.visible = false;
 	// actionArray.push(sprite_p_fxTop);
 	creatureAction.addChild(sprite_p_fxTop);
 
@@ -1202,23 +1202,26 @@ function createSprite(direction, item, index){
 
 	CustomEase.create("custom", "M0,0 C0,0 0.01158,0.37382 0.02895,0.59744 0.03199,0.63651 0.03945,0.66471 0.05428,0.69882 0.06786,0.73005 0.08443,0.75214 0.10756,0.77829 0.12925,0.80281 0.14837,0.81604 0.17595,0.83638 0.2018,0.85545 0.21847,0.86832 0.24711,0.88122 0.30415,0.90691 0.34361,0.92278 0.40429,0.93921 0.45566,0.95312 0.48924,0.95608 0.54432,0.9617 0.72192,0.97982 1,1 1,1 ");
 
+	var anim1 = 0.33;
+	var anim2 = 1;
+
 	pAtkTween = new TimelineMax({paused: true});
 	pAtkTween.to(sprite_p_ready, 0, {alpha:1});
-	pAtkTween.fromTo(sprite_p_ready, 0.33, {x:0, y:0},{ease:"custom", x:-50, y:0, onComplete: function(){
+	pAtkTween.fromTo(sprite_p_ready, anim1, {x:0, y:0},{ease:"custom", x:-50, y:0, onComplete: function(){
 		sprite_p_ready.alpha = 0;
 		sprite_p_attack.visible = true;
 		sprite_p_fxBack.visible = true;
 		sprite_p_fxTop.visible = true;
 	}});
-	pAtkTween.fromTo(sprite_p_fxBack, 1, {x:0, y:0}, {ease:"custom", x: 125, y:0});
-	pAtkTween.fromTo(sprite_p_attack, 1, {x:0, y:0}, {ease:"custom", x: 200, y:0}, 0.33);
-	pAtkTween.fromTo(sprite_p_fxTop, 1, {x:0, y:0}, {ease:"custom", x: 275, y:0, onComplete: function(){
+	pAtkTween.fromTo(sprite_p_fxBack, anim2, {x:0, y:0}, {ease:"custom", x: 125, y:0});
+	pAtkTween.fromTo(sprite_p_attack, anim2, {x:0, y:0}, {ease:"custom", x: 200, y:0}, anim1);
+	pAtkTween.fromTo(sprite_p_fxTop, anim2, {x:0, y:0}, {ease:"custom", x: 275, y:0, onComplete: function(){
 		sprite_p_fxBack.visible = false;
 		sprite_p_attack.visible = false;
 		sprite_p_fxTop.visible = false;
 		creatureAction.visible = false;
 		actionContainer.removeChild(creatureAction);
-	}},0.33);
+	}},anim1);
 
 	creatureAction.pAtkTween = pAtkTween;
 
@@ -1236,7 +1239,7 @@ function createSprite(direction, item, index){
 
 	creatureAction.dMissTween = dMissTween;
 
-	creatureAction.visible = false;
+	// creatureAction.visible = false;
 
 	// spriteReady.visible = false;
 	// creatureAction.addChild(spriteReady);
