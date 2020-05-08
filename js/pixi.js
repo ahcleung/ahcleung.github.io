e834abe1ea7ddf48ea2270edacd1e60609799271
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-05-07 22:52:37 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -1207,8 +1207,8 @@ function createSprite(direction, item, index){
 		sprite_p_fxBack.visible = true;
 		sprite_p_fxTop.visible = true;
 	}});
-	pAtkTween.fromTo(sprite_p_fxBack, anim2, {x:225, y:-150}, {ease:"custom", x:250, y:-150});
-	pAtkTween.fromTo(sprite_p_attack, anim2, {x:375, y:-75}, {ease:"custom", x:475, y:-75}, anim1);
+	pAtkTween.fromTo(sprite_p_fxBack, anim2, {x:225, y:-150}, {ease:"custom", x:300, y:-150});
+	pAtkTween.fromTo(sprite_p_attack, anim2, {x:375, y:-75}, {ease:"custom", x:500, y:-75}, anim1);
 	pAtkTween.fromTo(sprite_p_fxTop, anim2, {x:200, y:0}, {ease:"custom", x:500, y:0, onComplete: function(){
 		sprite_p_fxBack.visible = false;
 		sprite_p_attack.visible = false;
