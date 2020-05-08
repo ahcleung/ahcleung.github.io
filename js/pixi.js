2cf674f199ac1c14399e76e0f94339c2154164bb
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-05-07 21:48:01 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -1197,7 +1197,7 @@ function createSprite(direction, item, index){
 	CustomEase.create("custom", "M0,0 C0,0 0.01158,0.37382 0.02895,0.59744 0.03199,0.63651 0.03945,0.66471 0.05428,0.69882 0.06786,0.73005 0.08443,0.75214 0.10756,0.77829 0.12925,0.80281 0.14837,0.81604 0.17595,0.83638 0.2018,0.85545 0.21847,0.86832 0.24711,0.88122 0.30415,0.90691 0.34361,0.92278 0.40429,0.93921 0.45566,0.95312 0.48924,0.95608 0.54432,0.9617 0.72192,0.97982 1,1 1,1 ");
 
 	var anim1 = 0.33;
-	var anim2 = 1;
+	var anim2 = 10;
 
 	pAtkTween = new TimelineMax({paused: true});
 	pAtkTween.to(sprite_p_ready, 0, {alpha:1});
@@ -1207,9 +1207,9 @@ function createSprite(direction, item, index){
 		sprite_p_fxBack.visible = true;
 		sprite_p_fxTop.visible = true;
 	}});
-	pAtkTween.fromTo(sprite_p_fxBack, anim2, {x:-400, y:0}, {ease:"custom", x: -350, y:0});
-	pAtkTween.fromTo(sprite_p_attack, anim2, {x:200, y:0}, {ease:"custom", x: 300, y:0}, anim1);
-	pAtkTween.fromTo(sprite_p_fxTop, anim2, {x:750, y:0}, {ease:"custom", x: 1000, y:0, onComplete: function(){
+	pAtkTween.fromTo(sprite_p_fxBack, anim2, {x:-400, y:0}, {ease:"custom", x: -400, y:0});
+	pAtkTween.fromTo(sprite_p_attack, anim2, {x:0, y:0}, {ease:"custom", x: 0, y:0}, anim1);
+	pAtkTween.fromTo(sprite_p_fxTop, anim2, {x:0, y:0}, {ease:"custom", x: 0, y:0, onComplete: function(){
 		sprite_p_fxBack.visible = false;
 		sprite_p_attack.visible = false;
 		sprite_p_fxTop.visible = false;
@@ -3203,7 +3203,7 @@ function onAdditionalDown(){
 	console.log("Additional");
 	additionalContainer.visible = true;
 
-	actionContainer.addChild(heroActionArray[0]);
+	actionContainer.addChild(heroActionArray[1]);
 }
 
 function onAdditionalCancelDown(){
