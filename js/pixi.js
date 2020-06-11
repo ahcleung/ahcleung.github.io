4cbbee7679d7e66271a924fb5a2f3235c58271b1
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-06-11 17:20:34 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -10,7 +10,177 @@
 *Description:     Project Elements production code
 *
 ======================================================================================*/
+let Application = PIXI.Application,
+	loader = PIXI.Loader.shared,
+	resources = PIXI.Loader.shared.resources,
+	Sprite = PIXI.Sprite,
+	Text = PIXI.Text;
 
+const app = new Application({
+	autoResize: true, transparent: true, resolution: devicePixelRatio || 1,
+});
+document.querySelector('#frame').appendChild(app.view);
+
+loader
+	.add([
+		"img/ability_move.png",
+		"img/additional.png",
+		"img/additional_cancel.png",
+		"img/additional_item.png",
+		"img/additional_skip.png",
+		"img/leper.ability.five.png",
+		"img/actionLines.png",
+		"img/bg3.png",
+		"js/creatures.json",
+		"js/skills.json",
+		"js/elements.json",
+
+		{name:'gorilla3_p_ready', url:'img/gorilla3_p_ready.png'},
+		{name:'gorilla3_p_main', url:'img/gorilla3_p_main.png'},
+		{name:'gorilla3_p_back', url:'img/gorilla3_p_back.png'},
+		{name:'gorilla3_p_top', url:'img/gorilla3_p_top.png'},
+		{name:'gorilla3_s_ready', url:'img/gorilla3_s_ready.png'},
+		{name:'gorilla3_s_main', url:'img/gorilla3_s_main.png'},
+		{name:'gorilla3_s_back', url:'img/gorilla3_s_back.png'},
+		{name:'gorilla3_s_top', url:'img/gorilla3_s_top.png'},
+		{name:'gorilla3_d_ready', url:'img/gorilla3_d_ready.png'},
+		{name:'gorilla3_d_miss', url:'img/gorilla3_d_miss.png'},
+		{name:'gorilla3_d_dmg', url:'img/gorilla3_d_dmg.png'},
+
+		{name:'toad1_p_ready', url:'img/toad1_p_ready.png'},
+		{name:'toad1_p_main', url:'img/toad1_p_main.png'},
+		{name:'toad1_p_back', url:'img/toad1_p_back.png'},
+		{name:'toad1_p_top', url:'img/toad1_p_top.png'},
+		{name:'toad1_s_ready', url:'img/toad1_s_ready.png'},
+		{name:'toad1_s_main', url:'img/toad1_s_main.png'},
+		{name:'toad1_s_back', url:'img/toad1_s_back.png'},
+		{name:'toad1_s_top', url:'img/toad1_s_top.png'},
+		{name:'toad1_d_ready', url:'img/toad1_d_ready.png'},
+		{name:'toad1_d_miss', url:'img/toad1_d_miss.png'},
+		{name:'toad1_d_dmg', url:'img/toad1_d_dmg.png'},
+
+		{name:'toad3_p_ready', url:'img/toad3_p_ready.png'},
+		{name:'toad3_p_main', url:'img/toad3_p_main.png'},
+		{name:'toad3_p_back', url:'img/toad3_p_back.png'},
+		{name:'toad3_p_top', url:'img/toad3_p_top.png'},
+		{name:'toad3_s_ready', url:'img/toad3_s_ready.png'},
+		{name:'toad3_s_main', url:'img/toad3_s_main.png'},
+		{name:'toad3_s_back', url:'img/toad3_s_back.png'},
+		{name:'toad3_s_top', url:'img/toad3_s_top.png'},
+		{name:'toad3_d_ready', url:'img/toad3_d_ready.png'},
+		{name:'toad3_d_miss', url:'img/toad3_d_miss.png'},
+		{name:'toad3_d_dmg', url:'img/toad3_d_dmg.png'},
+
+		{name:'goat2_1_p_ready', url:'img/goat2_1_p_ready.png'},
+		{name:'goat2_1_p_main', url:'img/goat2_1_p_main.png'},
+		{name:'goat2_1_p_back', url:'img/goat2_1_p_back.png'},
+		{name:'goat2_1_p_top', url:'img/goat2_1_p_top.png'},
+		{name:'goat2_1_s_ready', url:'img/goat2_1_s_ready.png'},
+		{name:'goat2_1_s_main', url:'img/goat2_1_s_main.png'},
+		{name:'goat2_1_s_back', url:'img/goat2_1_s_back.png'},
+		{name:'goat2_1_s_top', url:'img/goat2_1_s_top.png'},
+		{name:'goat2_1_d_ready', url:'img/goat2_1_d_ready.png'},
+		{name:'goat2_1_d_miss', url:'img/goat2_1_d_miss.png'},
+		{name:'goat2_1_d_dmg', url:'img/goat2_1_d_dmg.png'},
+
+		{name:'goat2_2_p_ready', url:'img/goat2_2_p_ready.png'},
+		{name:'goat2_2_p_main', url:'img/goat2_2_p_main.png'},
+		{name:'goat2_2_p_back', url:'img/goat2_2_p_back.png'},
+		{name:'goat2_2_p_top', url:'img/goat2_2_p_top.png'},
+		{name:'goat2_2_s_ready', url:'img/goat2_2_s_ready.png'},
+		{name:'goat2_2_s_main', url:'img/goat2_2_s_main.png'},
+		{name:'goat2_2_s_back', url:'img/goat2_2_s_back.png'},
+		{name:'goat2_2_s_top', url:'img/goat2_2_s_top.png'},
+		{name:'goat2_2_d_ready', url:'img/goat2_2_d_ready.png'},
+		{name:'goat2_2_d_miss', url:'img/goat2_2_d_miss.png'},
+		{name:'goat2_2_d_dmg', url:'img/goat2_2_d_dmg.png'},
+
+		{name:'hel1_p_ready', url:'img/hel1_p_ready.png'},
+		{name:'hel1_p_main', url:'img/hel1_p_main.png'},
+		{name:'hel1_p_back', url:'img/hel1_p_back.png'},
+		{name:'hel1_p_top', url:'img/hel1_p_top.png'},
+		{name:'hel1_s_ready', url:'img/hel1_s_ready.png'},
+		{name:'hel1_s_main', url:'img/hel1_s_main.png'},
+		{name:'hel1_s_back', url:'img/hel1_s_back.png'},
+		{name:'hel1_s_top', url:'img/hel1_s_top.png'},
+		{name:'hel1_d_ready', url:'img/hel1_d_ready.png'},
+		{name:'hel1_d_miss', url:'img/hel1_d_miss.png'},
+		{name:'hel1_d_dmg', url:'img/hel1_d_dmg.png'},
+
+		{name:'hel2_p_ready', url:'img/hel2_p_ready.png'},
+		{name:'hel2_p_main', url:'img/hel2_p_main.png'},
+		{name:'hel2_p_back', url:'img/hel2_p_back.png'},
+		{name:'hel2_p_top', url:'img/hel2_p_top.png'},
+		{name:'hel2_s_ready', url:'img/hel2_s_ready.png'},
+		{name:'hel2_s_main', url:'img/hel2_s_main.png'},
+		{name:'hel2_s_back', url:'img/hel2_s_back.png'},
+		{name:'hel2_s_top', url:'img/hel2_s_top.png'},
+		{name:'hel2_d_ready', url:'img/hel2_d_ready.png'},
+		{name:'hel2_d_miss', url:'img/hel2_d_miss.png'},
+		{name:'hel2_d_dmg', url:'img/hel2_d_dmg.png'},
+
+		{name:'status_bleed', url:'img/status_bleed.png'},
+		{name:'status_buff', url:'img/status_buff.png'},
+		{name:'status_burned', url:'img/status_burned.png'},
+		{name:'status_debuff', url:'img/status_debuff.png'},
+		{name:'status_depressed', url:'img/status_depressed.png'},
+		{name:'status_guard', url:'img/status_guard.png'},
+		{name:'status_immune', url:'img/status_immune.png'},
+		{name:'status_paralyzed', url:'img/status_paralyzed.png'},
+		{name:'status_poisoned', url:'img/status_poisoned.png'},
+		{name:'status_recover', url:'img/status_recover.png'},
+		{name:'status_secured', url:'img/status_secured.png'},
+		{name:'status_silenced', url:'img/status_silenced.png'},
+		{name:'status_stunned', url:'img/status_stunned.png'},
+		{name:'status_critical', url:'img/status_critical.png'},
+		
+		{name:'element_flora', url:'img/element_flora.png'},
+		{name:'element_water', url:'img/element_water.png'},
+		{name:'element_fire', url:'img/element_fire.png'},
+		{name:'element_earth', url:'img/element_earth.png'},
+		{name:'element_lightning', url:'img/element_lightning.png'},
+		{name:'element_wind', url:'img/element_wind.png'},
+		{name:'element_toxic', url:'img/element_toxic.png'},
+		{name:'element_spirit', url:'img/element_spirit.png'},
+		{name:'element_shadow', url:'img/element_shadow.png'},
+		
+		{name:'gorilla3_skeleton', url:'img/gorilla3_ske.json'},
+		{name:'gorilla3_texture_json', url:'img/gorilla3_tex.json'},
+		{name:'gorilla3_texture_png', url:'img/gorilla3_tex.png'},
+		{name:'toad1_skeleton', url:'img/toad1_ske.json'},
+		{name:'toad1_texture_json', url:'img/toad1_tex.json'},
+		{name:'toad1_texture_png', url:'img/toad1_tex.png'},
+		{name:'toad3_skeleton', url:'img/toad3_ske.json'},
+		{name:'toad3_texture_json', url:'img/toad3_tex.json'},
+		{name:'toad3_texture_png', url:'img/toad3_tex.png'},
+		{name:'goat2_1_skeleton', url:'img/goat2_1_ske.json'},
+		{name:'goat2_1_texture_json', url:'img/goat2_1_tex.json'},
+		{name:'goat2_1_texture_png', url:'img/goat2_1_tex.png'},
+		{name:'goat2_2_skeleton', url:'img/goat2_2_ske.json'},
+		{name:'goat2_2_texture_json', url:'img/goat2_2_tex.json'},
+		{name:'goat2_2_texture_png', url:'img/goat2_2_tex.png'},
+		{name:'hel1_skeleton', url:'img/hel1_ske.json'},
+		{name:'hel1_texture_json', url:'img/hel1_tex.json'},
+		{name:'hel1_texture_png', url:'img/hel1_tex.png'},
+		{name:'hel2_skeleton', url:'img/hel2_ske.json'},
+		{name:'hel2_texture_json', url:'img/hel2_tex.json'},
+		{name:'hel2_texture_png', url:'img/hel2_tex.png'}
+	])
+	.on("progress", loadProgressHandler)
+	.load(setup);
+
+function loadProgressHandler(loader, resource) {
+
+  //Display the file `url` currently being loaded
+  console.log("loading: " + resource.url); 
+
+  //Display the percentage of files currently loaded
+  console.log("progress: " + loader.progress + "%"); 
+
+  //If you gave your files names as the first argument 
+  //of the `add` method, you can access them like this
+  //console.log("loading: " + resource.name);
+}
 /*
 *	Declare  variables
 */
