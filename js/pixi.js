5c159b80f4ed2dcdf319048bcc351a094bd07332
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-07-04 16:01:14 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -934,7 +934,7 @@ function createSprite(direction, item, index){
 	CustomEase.create("custom", "M0,0 C0,0 0.01158,0.37382 0.02895,0.59744 0.03199,0.63651 0.03945,0.66471 0.05428,0.69882 0.06786,0.73005 0.08443,0.75214 0.10756,0.77829 0.12925,0.80281 0.14837,0.81604 0.17595,0.83638 0.2018,0.85545 0.21847,0.86832 0.24711,0.88122 0.30415,0.90691 0.34361,0.92278 0.40429,0.93921 0.45566,0.95312 0.48924,0.95608 0.54432,0.9617 0.72192,0.97982 1,1 1,1 ");
 
 	anim1 = 0.5;
-	anim2 = 5;
+	anim2 = 1.25;
 
 	// pAtkTween = new TimelineMax({paused: true});
 	// pAtkTween.to(sprite_p_ready, 0, {alpha:1});
@@ -956,12 +956,12 @@ function createSprite(direction, item, index){
 		sprite_p_top.visible = true;
 		actionLines.visible = true;
 	}});
-	pAtkTween.fromTo(sprite_p_back, anim2, {x:item.action[1][0], y:item.action[1][1]}, {ease:"custom", x:item.action[1][2], y:item.action[1][3]});
-	pAtkTween.fromTo(sprite_p_back, 0.33, {alpha:0}, {alpha:1}, anim1);
-	pAtkTween.fromTo(sprite_p_back, 0.25, {alpha:1}, {alpha:0}, anim1+anim2-0.25);
+	pAtkTween.fromTo(sprite_p_back, anim2, {x:item.action[1][0], y:item.action[1][1]}, {ease:"custom", x:item.action[1][2], y:item.action[1][3]}, anim1);
+	pAtkTween.fromTo(sprite_p_back, 0.3, {alpha:0}, {alpha:1}, anim1);
+	pAtkTween.fromTo(sprite_p_back, 0.25, {alpha:1}, {alpha:0}, anim1+0.73);
 	pAtkTween.fromTo(sprite_p_main, anim2, {x:item.action[2][0], y:item.action[2][1]}, {ease:"custom", x:item.action[2][2], y:item.action[2][3]}, anim1);
-	pAtkTween.fromTo(sprite_p_top, 0.5, {alpha:0}, {alpha:1}, anim1);
-	pAtkTween.fromTo(sprite_p_top, 0.33, {alpha:1}, {alpha:0}, anim1+anim2-0.33);
+	pAtkTween.fromTo(sprite_p_top, 0.33, {alpha:0}, {alpha:1}, anim1);
+	pAtkTween.fromTo(sprite_p_top, 0.25, {alpha:1}, {alpha:0}, anim1+1);
 	pAtkTween.fromTo(sprite_p_top, anim2, {x:item.action[3][0], y:item.action[3][1]}, {ease:"custom", x:item.action[3][2], y:item.action[3][3], onComplete: function(){
 		sprite_p_back.visible = false;
 		sprite_p_main.visible = false;
