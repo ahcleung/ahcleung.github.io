73b52d7d445a19e9a5dc8140e390e35b5ee31be9
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-07-08 16:39:42 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -1106,7 +1106,7 @@ function createSprite(direction, item, index){
 	dmgPopupTween.to(dmgPopup, 0.267, {delay: 0.9, ease:Expo.easeInOut, y: 100, alpha: 0});
 	dmgPopupTween.to(dmgPopup.scale, 0.267, {delay: 0.9, ease:Expo.easeInOut, x: 0.5, y: 0.5}, 0.2+anim1);
 
-	dmgPopup.alpha = 0;
+	dmgPopup.alpha = 1;
 	dmgPopup.tween = dmgPopupTween;	
 	dmgPopup.addChild(dmgNum);
 	dmgPopup.dmgNum = dmgNum;
