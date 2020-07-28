fd9c3fee3ecc5db6a01c51798a2088ec32e8504c
ahcleung <31660669+ahcleung@users.noreply.github.com>
2020-07-28 17:32:50 -0400

    Add files via upload

--- js/pixi.js
+++ js/pixi.js
@@ -2318,7 +2318,7 @@ function moveCreature(movingCreature, displacement){
 
 function onHPDown(){
 	// console.log("HP:" + this.object.pos);
-	console.log("Name: " + this.object.name + " || Stats: " + this.object.statMod);
+	console.log("Name: " + this.object.name + "\nStats: " + this.object.statMod + "\nStatus: " + this.object.statusArray);
 }
 
 function onSkillDown(){
