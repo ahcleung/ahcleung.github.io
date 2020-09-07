24fe6647af57c92f9bf8e505a3fe2fafaec0432a
NOMADIC\Alvin <alvin.hc.leung@gmail.com>
2020-09-07 17:48:06 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -2889,7 +2889,7 @@ function animationSequence(attacker, defender, animateBattle, animatePopup, anim
 				item.visible = true;
 			});
 			if(animateStatus){
-				attacker.dmgContainer.dmgStatus.play(0);
+				attacker.dmgContainer.dmgStatus.tween.play(0);
 				defender.forEach(arrayCreature=>{
 					arrayCreature.dmgContainer.dmgStatus.tween.play(0);
 					arrayCreature.dmgContainer.dmgStatus.tween.eventCallback("onComplete", function(){
