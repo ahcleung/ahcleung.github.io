0ea5b5c81878349b758eb47bf21eef5d58cc4c13
ahcleung <alvin.hc.leung@gmail.com>
2020-09-13 16:24:37 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -2920,10 +2920,10 @@ function calculateDamage(attacker, defender, hitArray){
 				defense = targeted.sdef;
 			}
 			targeted.elements.forEach(element =>{
-				console.log("effectiveness: " + skillsList.data.skills[selectedSkill].element);
+				console.log("effectiveness: " + skillsList.data.skills[selectedSkill].element-1);
 				console.log("effectiveness: " + element);
-				console.log("effectiveness: " + elementList.data.elements[0]["effect"][element]);
-				// effectiveness *= elementList.data.elements[skillsList.data.skills[selectedSkill].element-1][element];
+				console.log("effectiveness: " + elementList.data.elements[skillsList.data.skills[selectedSkill].element-1]["effect"][element]);
+				effectiveness *= elementList.data.elements[skillsList.data.skills[selectedSkill].element-1]["effect"][element];
 			});
 			console.log("effectiveness: " + effectiveness);
 			if(effectiveness == 0.25 && !skillHeal){
