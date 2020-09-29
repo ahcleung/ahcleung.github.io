20c41afe7a4cd5d8add7a48d9d9044a8392ef815
ahcleung <alvin.hc.leung@gmail.com>
2020-09-29 13:43:13 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1906,7 +1906,9 @@ function resizeHP(roster, item){
 	item.healthBar.heal.pivot.x = item.healthBar.heal.width/2;
 	item.healthBar.heal.x = item.healthBar.heal.width/2;
 
-	item.healthBar.select.animate.invalidate();
+	if(item.healthBar.select.animate.isActive){
+		item.healthBar.select.animate.remove();
+	}	
 	var selectWidth = item.healthBar.select.width;
 	item.healthBar.select.animate.to(item.healthBar.select, 1, {width:selectWidth+20, ease:Sine.easeInOut});
 	item.healthBar.select.animate.to(item.healthBar.select, 0.5, {width:selectWidth, ease:Sine.easeInOut});
