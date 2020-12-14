44a6ccd016eaf729139486d2501e210577b188c4
ahcleung <alvin.hc.leung@gmail.com>
2020-12-14 17:43:45 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -3209,7 +3209,8 @@ function onHPDown(){
 			let spriteItem = new PIXI.Sprite(resources[itemList.data.item[item].code].texture);
 
 			const filter1 = new PIXI.filters.ColorMatrixFilter();
-			filter1.greyscale(0.5);
+			filter1.desaturate();
+			// filter1.greyscale(0.5);
 			spriteItem.filters = [filter1];
 
 			spriteItem.width = infoItemWidth-skillSelectPadding*2;
