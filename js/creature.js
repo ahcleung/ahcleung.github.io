138de6e408bf7c7538531d348e0c810ac32c6fcd
ahcleung <alvin.hc.leung@gmail.com>
2020-12-14 17:47:25 -0500

    Update creature.js

--- js/creature.js
+++ js/creature.js
@@ -27,7 +27,7 @@ class Creature{
 		this.newCrit = false;
 		this.newHP = false;
 		this.newMove = 0;
-		this.item = [2,2];
+		this.item = [1];
 		
 		const creatureList = resources["js/creature.json"];				//Load creature JSON list
 		
