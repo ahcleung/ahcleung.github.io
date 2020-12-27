6c46e41e3061e68e52c051346eb797a5c6d2b48f
NOMADIC\Alvin <alvin.hc.leung@gmail.com>
2020-12-26 23:09:59 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -3283,8 +3283,8 @@ function onHPDown(){
 
 	creatureInfo.info_stat_text.forEach((text,textIndex) =>{
 		text.style.fontSize = skillNameFontSize;
-		text.x = (textIndex%3) * app.screen.width/15;
-		text.y = Math.floor(textIndex/3) * app.screen.height/13;
+		text.x = (textIndex%3) * app.screen.width/10;
+		text.y = Math.floor(textIndex/3) * app.screen.height/12;
 	});
 
 	// if(skillIndex%2 == 0){
