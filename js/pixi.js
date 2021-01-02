d5a2f7102c8b59a1c05f2c632678a715896bdcd9
NOMADIC\Alvin <alvin.hc.leung@gmail.com>
2021-01-02 01:46:52 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -3360,9 +3360,11 @@ function onHPDown(){
 		creatureInfo.info_stat_maxed[increaseIndex].y = (increaseIndex+1) * app.screen.height/12;
 		if(this.object.statDis[increaseIndex] == 150){
 			increase.visible = false;
+			creatureInfo.info_stat_maxed[increaseIndex].visible = true;
 			creatureInfo.info_stat_text[increaseIndex*3+5].style.fill = '#FFd600';
 		}else{
 			increase.visible = true;
+			creatureInfo.info_stat_maxed[increaseIndex].visible = false;
 			creatureInfo.info_stat_text[increaseIndex*3+5].style.fill = '#fefefe';
 		}
 	});
