6e2cacd9a0347a2ad9b8dd262a9518d4feac6678
NOMADIC\Alvin <alvin.hc.leung@gmail.com>
2021-01-01 21:44:19 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -292,13 +292,13 @@ const hero = [];
 hero[0] = {
 	id: 6, level: 50, 
 	skill1: 4, skill2: 1, skill3: 3, skill4: 2,
-	statDODG: 20, statHP: 0, statPATK: 10, statPDEF: 0, statSATK: 0, statSDEF: 0, statSPD: 0,
+	statDODG: 20, statHP: 0, statPATK: 10, statPDEF: 0, statSATK: 115, statSDEF: 0, statSPD: 0,
 	hero: true
 };
 hero[1] = {
 	id: 11, level: 47, 
 	skill1: 1, skill2: 10, skill3: 11, skill4: 3,
-	statDODG: 95, statHP: 0, statPATK: 0, statPDEF: 3, statSATK: 0, statSDEF: 20, statSPD: 0,
+	statDODG: 95, statHP: 0, statPATK: 0, statPDEF: 21, statSATK: 0, statSDEF: 20, statSPD: 0,
 	hero: true
 };
 hero[2] = {
@@ -317,19 +317,19 @@ const enemy = [];
 enemy[0] = {
 	id: 9, level: 49, 
 	skill1: 4, skill2: 6, skill3: 1, skill4: 2,
-	statDODG: 70, statHP: 20, statPATK: 0, statPDEF: 40, statSATK: 60, statSDEF: 0, statSPD: 60,
+	statDODG: 70, statHP: 12, statPATK: 0, statPDEF: 0, statSATK: 0, statSDEF: 0, statSPD: 60,
 	hero: false
 };
 enemy[1] = {
 	id: 8, level: 46, 
 	skill1: 4, skill2: 10, skill3: 11, skill4: 2,
-	statDODG: 10, statHP: 0, statPATK: 0, statPDEF: 0, statSATK: 0, statSDEF: 0, statSPD: 0,
+	statDODG: 10, statHP: 0, statPATK: 0, statPDEF: 0, statSATK: 0, statSDEF: 120, statSPD: 0,
 	hero: false
 };
 enemy[2] = {
 	id: 12, level: 45, 
 	skill1: 4, skill2: 1, skill3: 5, skill4: 2,
-	statDODG: 0, statHP: 0, statPATK: 0, statPDEF: 0, statSATK: 40, statSDEF: 0, statSPD: 0,
+	statDODG: 0, statHP: 0, statPATK: 0, statPDEF: 85, statSATK: 40, statSDEF: 0, statSPD: 0,
 	hero: false
 };
 // enemy[3] = {
