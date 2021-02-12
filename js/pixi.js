f64732e2f503f6b3a17aab44b0d6dd98f27e8891
ahcleung <alvin.hc.leung@gmail.com>
2021-02-11 20:58:00 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1406,7 +1406,7 @@ function createSprite(direction, item, index){
 
 	var sprite_d_ready = new PIXI.Sprite(resources[item.code + '_d_ready'].texture);
 	sprite_d_ready.anchor.set(1);
-	sprite_d_ready.alpha = 0;
+	// sprite_d_ready.alpha = 0;
 	
 	creatureAction.addChild(sprite_d_ready);
 	sprite_d_ready.position.set(500,0);
