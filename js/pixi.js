4c82a9cd70e1f9a9f851fe82843ad64376e92c16
ahcleung <alvin.hc.leung@gmail.com>
2021-02-12 17:41:04 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1234,14 +1234,14 @@ function setup(){
 
 	resize();
 
-	enemyArray[2].action.sprite_s_ready.x = -100;
-	enemyArray[2].action.sprite_s_ready.y = 0;
-	// enemyArray[2].action.sprite_s_back.x = 200;
-	// enemyArray[2].action.sprite_s_back.y = -75;
+	// enemyArray[2].action.sprite_s_ready.x = -100;
+	// enemyArray[2].action.sprite_s_ready.y = 0;
+	enemyArray[2].action.sprite_s_back.x = 0;
+	enemyArray[2].action.sprite_s_back.y = 0;
 	enemyArray[2].action.sprite_s_main.x = 100;
 	enemyArray[2].action.sprite_s_main.y = 0;
-	// enemyArray[2].action.sprite_s_top.x = 800;
-	// enemyArray[2].action.sprite_s_top.y = 35;
+	enemyArray[2].action.sprite_s_top.x = 0;
+	enemyArray[2].action.sprite_s_top.y = 0;
 
 	state = play;
 	
@@ -1401,8 +1401,8 @@ function createSprite(direction, item, index){
 
 	var sprite_s_back = new PIXI.Sprite(resources[item.code + '_s_back'].texture);
 	sprite_s_back.anchor.set(1);
-	sprite_s_back.alpha = 0;
-	sprite_s_back.visible = false;
+	// sprite_s_back.alpha = 0;
+	// sprite_s_back.visible = false;
 	creatureAction.sprite_s_back = sprite_s_back;
 	creatureAction.addChild(sprite_s_back);
 	creatureAction.fxBack = sprite_s_back;
@@ -1415,8 +1415,8 @@ function createSprite(direction, item, index){
 
 	var sprite_s_top = new PIXI.Sprite(resources[item.code + '_s_top'].texture);
 	sprite_s_top.anchor.set(1);
-	sprite_s_top.alpha = 0;
-	sprite_s_top.visible = false;
+	// sprite_s_top.alpha = 0;
+	// sprite_s_top.visible = false;
 	creatureAction.sprite_s_top = sprite_s_top;
 	creatureAction.addChild(sprite_s_top);
 	creatureAction.fxTop = sprite_s_top;
