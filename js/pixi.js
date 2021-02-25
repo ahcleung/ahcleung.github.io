2f2fac8d5bee46b3e2fcef84503ed51bdd3b2f27
ahcleung <alvin.hc.leung@gmail.com>
2021-02-25 18:06:50 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1277,7 +1277,7 @@ function setup(){
 	turnText.visible = false;
 	interfaceHolder.visible = false;
 
-	var playerPos = [41,15];
+	var playerPos = [42,15];
 	// var playerPos = [0,1];
 
 	for(var i = 0; i < 44; i++){
