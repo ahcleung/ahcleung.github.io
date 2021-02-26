bbcd449049465764def5085ea84a58624b681b5e
ahcleung <alvin.hc.leung@gmail.com>
2021-02-26 17:54:45 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1429,13 +1429,13 @@ function setup(){
 
 	app.stage.addChild(mapHolder);
 
-	var centerGraphic = new PIXI.Graphics();
-	centerGraphic.beginFill(0xff0000);
-	centerGraphic.drawRect(0, 0, 100, 100);
-	centerGraphic.endFill();
-	app.stage.addChild(centerGraphic);
-	centerGraphic.x = app.screen.width/2;
-	centerGraphic.y = app.screen.height/2;
+	// var centerGraphic = new PIXI.Graphics();
+	// centerGraphic.beginFill(0xff0000);
+	// centerGraphic.drawRect(0, 0, 100, 100);
+	// centerGraphic.endFill();
+	// app.stage.addChild(centerGraphic);
+	// centerGraphic.x = app.screen.width/2;
+	// centerGraphic.y = app.screen.height/2;
 
 	// mapHolder
 
