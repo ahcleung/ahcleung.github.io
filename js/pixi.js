05d21efd6aae2e8e43d375cdf82fbc5bc64d779a
ahcleung <alvin.hc.leung@gmail.com>
2021-02-26 17:39:44 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1402,7 +1402,7 @@ function setup(){
 	// var tileSize = [mapHolder.width/50,mapHolder.width/44];
 	mapHolder.x = -(sizeTile.width*3/4 * playerPos[0]) - (sizeTile.width/2) + (app.screen.width/2);
 	if(playerPos[0]% 2 == 0){		
-		mapHolder.y = -(sizeTile.height*0.8625 * playerPos[1]) - (sizeTile.height/2) + (app.screen.height/2);
+		mapHolder.y = -(sizeTile.height*0.8625 * playerPos[1]) + (sizeTile.height/2) + (app.screen.height/2);
 	}else{
 		mapHolder.y = -(sizeTile.height*0.8625 * playerPos[1]) - (sizeTile.height/2) + (app.screen.height/2);
 	}
