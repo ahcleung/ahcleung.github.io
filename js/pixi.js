e8ea4587b8e5d5a126f11cfaa1d8ac90c27c4d99
ahcleung <alvin.hc.leung@gmail.com>
2021-02-28 21:30:33 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1541,11 +1541,12 @@ function showTraversable(){
 		(playerPos[1] * 50) + (playerPos[0]-1)
 	];
 	if(playerPos[0]% 2 == 0){
-		traversableIndex.push(((playerPos[1]-1) * 50) + (playerPos[0]+1));
-		traversableIndex.push(((playerPos[1]+1) * 50) + (playerPos[0]+1));
-	}else{
+		
 		traversableIndex.push(((playerPos[1]-1) * 50) + (playerPos[0]-1));
 		traversableIndex.push(((playerPos[1]+1) * 50) + (playerPos[0]-1));
+	}else{
+		traversableIndex.push(((playerPos[1]-1) * 50) + (playerPos[0]+1));
+		traversableIndex.push(((playerPos[1]+1) * 50) + (playerPos[0]+1));
 	}
 
 	traversableIndex.forEach(arrayIndex=>{
