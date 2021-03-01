aa3b5420bbdc66bc0de376ba8e49a9636dc617ba
ahcleung <alvin.hc.leung@gmail.com>
2021-03-01 08:36:44 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1399,7 +1399,7 @@ function setup(){
 	// app.stage.addChild(tileArray[0].sprite);
 
 	var centerGraphic = new PIXI.Graphics();
-	centerGraphic.beginFill(0xff0000);
+	centerGraphic.beginFill(0x00ff00);
 	centerGraphic.drawRect(0, 0, 100, 100);
 	centerGraphic.endFill();
 	app.stage.addChild(centerGraphic);
