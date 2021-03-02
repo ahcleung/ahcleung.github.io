02293116029233e204c26d0fdf983223682c89ec
ahcleung <alvin.hc.leung@gmail.com>
2021-03-01 22:23:47 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1623,7 +1623,7 @@ function showTraversable(){
 	removeFromArray = [];
 	traversableRing2.forEach((tileIndex, index)=>{
 		removeArray.forEach(pos=>{
-			if(tileArray[tileIndex].pos == pos){
+			if(tileArray[tileIndex].pos[0] == pos[0] && tileArray[tileIndex].pos[1] == pos[1]){
 				removeFromArray.push(index);
 			}
 		});		
@@ -1639,6 +1639,22 @@ function showTraversable(){
 		tileArray[arrayIndex].showMove1();
 		tileTraversable.push(tileArray[arrayIndex]);
 	});
+}
+
+function arraysEqual(a, b) {
+	if (a === b) return true;
+	if (a == null || b == null) return false;
+	if (a.length !== b.length) return false;
+
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+  // Please note that calling sort on an array will modify that array.
+  // you might want to clone your array first.
+
+	for (var i = 0; i < a.length; ++i) {
+		if (a[i] !== b[i]) return false;
+	}
+	return true;
 }
 
 function addMoveTile(tileContainer){
