a1535e6cf771bde6f5351128929372fb12c8ac76
ahcleung <alvin.hc.leung@gmail.com>
2021-03-03 22:11:54 -0500

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1318,75 +1318,87 @@ function setup(){
 	// 	}
 	// }
 
-	for(var i = 0; i < 46; i++){
-		for(var j = 0; j < 52; j++){
-			if(i == 0 && j == 0){
-				const newTile = new Tile({
-					id: 16,
-					pos: [j,i],
-					discovered: false,
-					travelled: false
-				});
-			}else if(i == 0 && j== 51){
-				const newTile = new Tile({
-					id: 10,
-					pos: [j,i],
-					discovered: false,
-					travelled: false
-				});
-			}else if(i == 45 && j == 0){
-				const newTile = new Tile({
-					id: 14,
-					pos: [j,i],
-					discovered: false,
-					travelled: false
-				});
-			}else if(i == 45 && j == 51){
-				const newTile = new Tile({
-					id: 12,
-					pos: [j,i],
-					discovered: false,
-					travelled: false
-				});
-			}else if(i == 0){
-				const newTile = new Tile({
-					id: 9,
-					pos: [j,i],
-					discovered: false,
-					travelled: false
-				});
-			}else if(i == 45){
-				const newTile = new Tile({
-					id: 13,
-					pos: [j,i],
-					discovered: false,
-					travelled: false
-				});
-			}else if(j == 0){
-				const newTile = new Tile({
-					id: 15,
-					pos: [j,i],
-					discovered: false,
-					travelled: false
-				});
-			}else if(j == 51){
-				const newTile = new Tile({
-					id: 11,
-					pos: [j,i],
-					discovered: false,
-					travelled: false
-				});
-			}else{
-				const newTile = new Tile({
-					id: mapList.data.maps[0].tiles[i][j],
-					pos: [j,i],
-					discovered: false,
-					travelled: false
-				});
-			}
+	for(var i = 0; i < 44; i++){
+		for(var j = 0; j < 50; j++){
+			const newTile = new Tile({
+				id: 9,
+				pos: [j,i],
+				discovered: false,
+				travelled: false
+			});
 			tileArray.push(newTile);
 		}
 	}
+
+	// for(var i = 0; i < 46; i++){
+	// 	for(var j = 0; j < 52; j++){
+	// 		if(i == 0 && j == 0){
+	// 			const newTile = new Tile({
+	// 				id: 16,
+	// 				pos: [j,i],
+	// 				discovered: false,
+	// 				travelled: false
+	// 			});
+	// 		}else if(i == 0 && j== 51){
+	// 			const newTile = new Tile({
+	// 				id: 10,
+	// 				pos: [j,i],
+	// 				discovered: false,
+	// 				travelled: false
+	// 			});
+	// 		}else if(i == 45 && j == 0){
+	// 			const newTile = new Tile({
+	// 				id: 14,
+	// 				pos: [j,i],
+	// 				discovered: false,
+	// 				travelled: false
+	// 			});
+	// 		}else if(i == 45 && j == 51){
+	// 			const newTile = new Tile({
+	// 				id: 12,
+	// 				pos: [j,i],
+	// 				discovered: false,
+	// 				travelled: false
+	// 			});
+	// 		}else if(i == 0){
+	// 			const newTile = new Tile({
+	// 				id: 9,
+	// 				pos: [j,i],
+	// 				discovered: false,
+	// 				travelled: false
+	// 			});
+	// 		}else if(i == 45){
+	// 			const newTile = new Tile({
+	// 				id: 13,
+	// 				pos: [j,i],
+	// 				discovered: false,
+	// 				travelled: false
+	// 			});
+	// 		}else if(j == 0){
+	// 			const newTile = new Tile({
+	// 				id: 15,
+	// 				pos: [j,i],
+	// 				discovered: false,
+	// 				travelled: false
+	// 			});
+	// 		}else if(j == 51){
+	// 			const newTile = new Tile({
+	// 				id: 11,
+	// 				pos: [j,i],
+	// 				discovered: false,
+	// 				travelled: false
+	// 			});
+	// 		}else{
+	// 			const newTile = new Tile({
+	// 				id: mapList.data.maps[0].tiles[i][j],
+	// 				pos: [j,i],
+	// 				discovered: false,
+	// 				travelled: false
+	// 			});
+	// 		}
+	// 		tileArray.push(newTile);
+	// 	}
+	// }
 
 	tileArray.forEach((item, itemIndex) =>{
 		createTile(item, itemIndex)
