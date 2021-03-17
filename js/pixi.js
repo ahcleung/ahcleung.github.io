4cb555e9548ac5ff90a53ece661b528ccbe1e434
ahcleung <alvin.hc.leung@gmail.com>
2021-03-16 20:43:10 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1690,16 +1690,20 @@ function showTraversable(){
 				// xPos = playerPos[0]+i
 				console.log("Block: [" + xPos + ", " + yPos + "]");
 
-				if(i == 0){
-					yCount = count-1;
-				}else{
-					yCount = count;
-				}
-				for(j = 0; j < yCount; j++){
-					var newY;
-					if(bottom)		newY = yPos-j;
-					else  			newY = yPos+j;
-					console.log("Block: [" + xPos + ", " + newY + "]");
+				if(Math.abs(mountainDifference[0]) == 1 && mountainDifference[1] == 1){
+				}else if(Math.abs(mountainDifference[0]) == 1 && mountainDifference[1] == -2){
+				}else{}
+					if(i == 0){
+						yCount = count-1;
+					}else{
+						yCount = count;
+					}
+					for(j = 0; j < yCount; j++){
+						var newY;
+						if(bottom)		newY = yPos-j;
+						else  			newY = yPos+j;
+						console.log("Block: [" + xPos + ", " + newY + "]");
+					}
 				}
 
 				removeArray.push([xPos,yPos]);
