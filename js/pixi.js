12efbcfe6e44a86dd5b454b2060673828c1dbee8
ahcleung <alvin.hc.leung@gmail.com>
2021-05-04 16:23:55 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -1502,7 +1502,7 @@ function setup(){
 	textRect.beginFill(0x222222);
 	textRect.drawRect(margin, 0, app.screen.width-(2*margin), (app.screen.height/3)-margin);
 	textRect.endFill();
-	textRect.alpha = 0.75;
+	textRect.alpha = 0.95;
 	encounterTextBox.addChild(textRect);
 	encounterTextBox.rect = textRect;
 
