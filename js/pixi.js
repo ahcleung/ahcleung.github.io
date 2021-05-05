16b33f2027cf8f9b3b045f2c98ff5d196050c579
ahcleung <alvin.hc.leung@gmail.com>
2021-05-05 14:20:50 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -3224,8 +3224,8 @@ function resizeEncounter(){
 	// -25, -25, app.screen.width+50, app.screen.height+50
 	encounterHolder.bg.x = 10;
 	encounterHolder.bg.y = 10;
-	encounterHolder.bg.width = app.screen.width;
-	encounterHolder.bg.height = app.screen.height;
+	encounterHolder.bg.width = app.screen.width-10;
+	encounterHolder.bg.height = app.screen.height-10;
 	// encounterHolder.bg.width = app.screen.width;
 	// encounterHolder.bg.height = app.screen.height;
 }
