3468c0af4abd949c11cd6f768575c6d44b3cb8c7
ahcleung <alvin.hc.leung@gmail.com>
2021-05-12 22:35:01 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -3487,11 +3487,11 @@ function resizeInfo(){
 	creatureInfo.info_stat_text.forEach((text,textIndex) =>{
 		text.style.fontSize = skillNameFontSize;
 		if(textIndex < 2){
-			text.x = ((textIndex%3)+1) * app.screen.width/12 + app.screen.width/50;
+			text.x = ((textIndex%3)+1) * app.screen.width/13 + app.screen.width/50;
 		}else if(textIndex == 2){
 			text.x = ((textIndex%3)+1) * app.screen.width/11 + app.screen.width/50;
 		}else if(textIndex%3 != 0){
-			text.x = (textIndex%3) * app.screen.width/12 + app.screen.width/50;
+			text.x = (textIndex%3) * app.screen.width/13 + app.screen.width/50;
 			text.y = Math.floor(textIndex/3) * app.screen.height/12;
 		}else{
 			text.x = app.screen.width/15;
@@ -4962,11 +4962,11 @@ function onHPDown(){
 	creatureInfo.info_stat_text.forEach((text,textIndex) =>{
 		text.style.fontSize = skillNameFontSize;
 		if(textIndex < 2){
-			text.x = ((textIndex%3)+1) * app.screen.width/12 + app.screen.width/50;
+			text.x = ((textIndex%3)+1) * app.screen.width/13 + app.screen.width/50;
 		}else if(textIndex == 2){
 			text.x = ((textIndex%3)+1) * app.screen.width/11 + app.screen.width/50;
 		}else if(textIndex%3 != 0){
-			text.x = (textIndex%3) * app.screen.width/12 + app.screen.width/50;
+			text.x = (textIndex%3) * app.screen.width/13 + app.screen.width/50;
 			text.y = Math.floor(textIndex/3) * app.screen.height/12;
 		}else{
 			text.x = app.screen.width/15;
