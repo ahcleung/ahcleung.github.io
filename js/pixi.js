4cc18947a6e208dcb0a57192b82d46f546f132de
NOMADIC\Alvin <alvin.hc.leung@gmail.com>
2021-05-28 20:58:18 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -4938,6 +4938,20 @@ function onSkillDown(){
 							}
 						}
 					});
+				}else{
+					heroArray.forEach(arrayCreature =>{
+						if(arrayCreature.size == 1){
+							if(arrayCreature.pos == spotIndex+1 || arrayCreature.pos == spotIndex+2){
+								newTargets.push(arrayCreature);
+							}
+						}else if(arrayCreature.size == 2){
+							var pos1 = arrayCreature.pos;
+							var pos2 = arrayCreature.pos + 1;
+							if(pos1 == spotIndex+1 || pos2 == spotIndex+1 || pos1 == spotIndex+2 || pos2 == spotIndex+2){
+								newTargets.push(arrayCreature);	
+							}
+						}
+					});
 				}
 			}
 		});
