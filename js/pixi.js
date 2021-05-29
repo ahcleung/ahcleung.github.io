8f01b94425dbd7395ea2b8479a21049df75d52e7
NOMADIC\Alvin <alvin.hc.leung@gmail.com>
2021-05-29 16:24:32 -0400

    Update pixi.js

--- js/pixi.js
+++ js/pixi.js
@@ -4752,31 +4752,35 @@ function onSkillDown(){
 		validSkillObjectArray.push(columnObjectArray);
 	}
 
-	if(selectedVita.hero){
-		if(team){
-			heroArray.forEach(arrayCreature =>{
-				var targeted = false;
-				skillList.data.skill[this.identifier[1]].target.forEach((skillTarget, skillTargetIndex)=> {
-					if(skillTarget == 1 && !targeted){
-						var posTracker = skillTargetIndex+1;
-						if(arrayCreature.size == 2){
-							var pos1 = arrayCreature.pos;
-							var pos2 = arrayCreature.pos + 1;
-							if(posTracker == pos1 || posTracker == pos2){
-								validSkillObjectArray.push([arrayCreature]);
-								targeted = true;
-							}
-						}else{
-							if(posTracker == arrayCreature.pos){
-								validSkillObjectArray.push([arrayCreature]);
-								targeted = true;
-							}
-						}
-					}
-				});
-			});
-		}
-		else{
+	var targetEnemy = true;
+	if(selectedVita.hero && team)		targetEnemy = false;
+	if(!selectedVita.hero && !team)		targetEnemy = false;
+
+	if(targetEnemy){
+		// if(team){
+		// 	heroArray.forEach(arrayCreature =>{
+		// 		var targeted = false;
+		// 		skillList.data.skill[this.identifier[1]].target.forEach((skillTarget, skillTargetIndex)=> {
+		// 			if(skillTarget == 1 && !targeted){
+		// 				var posTracker = skillTargetIndex+1;
+		// 				if(arrayCreature.size == 2){
+		// 					var pos1 = arrayCreature.pos;
+		// 					var pos2 = arrayCreature.pos + 1;
+		// 					if(posTracker == pos1 || posTracker == pos2){
+		// 						validSkillObjectArray.push([arrayCreature]);
+		// 						targeted = true;
+		// 					}
+		// 				}else{
+		// 					if(posTracker == arrayCreature.pos){
+		// 						validSkillObjectArray.push([arrayCreature]);
+		// 						targeted = true;
+		// 					}
+		// 				}
+		// 			}
+		// 		});
+		// 	});
+		// }
+		// else{
 			enemyArray.forEach(arrayCreature =>{
 				var targeted = false;
 				skillList.data.skill[this.identifier[1]].target.forEach((skillTarget, skillTargetIndex)=> {
@@ -4798,31 +4802,31 @@ function onSkillDown(){
 					}
 				});
 			});
-		}
+		// }
 	}else{
-		if(team){
-			enemyArray.forEach(arrayCreature =>{
-				var targeted = false;
-				skillList.data.skill[this.identifier[1]].target.forEach((skillTarget, skillTargetIndex)=> {
-					if(skillTarget == 1 && !targeted){
-						var posTracker = skillTargetIndex+1;
-						if(arrayCreature.size == 2){
-							var pos1 = arrayCreature.pos;
-							var pos2 = arrayCreature.pos + 1;
-							if(posTracker == pos1 || posTracker == pos2){
-								validSkillObjectArray.push([arrayCreature]);
-								targeted = true;
-							}
-						}else{
-							if(posTracker == arrayCreature.pos){
-								validSkillObjectArray.push([arrayCreature]);
-								targeted = true;
-							}
-						}
-					}
-				});
-			});	
-		}else{
+		// if(team){
+		// 	enemyArray.forEach(arrayCreature =>{
+		// 		var targeted = false;
+		// 		skillList.data.skill[this.identifier[1]].target.forEach((skillTarget, skillTargetIndex)=> {
+		// 			if(skillTarget == 1 && !targeted){
+		// 				var posTracker = skillTargetIndex+1;
+		// 				if(arrayCreature.size == 2){
+		// 					var pos1 = arrayCreature.pos;
+		// 					var pos2 = arrayCreature.pos + 1;
+		// 					if(posTracker == pos1 || posTracker == pos2){
+		// 						validSkillObjectArray.push([arrayCreature]);
+		// 						targeted = true;
+		// 					}
+		// 				}else{
+		// 					if(posTracker == arrayCreature.pos){
+		// 						validSkillObjectArray.push([arrayCreature]);
+		// 						targeted = true;
+		// 					}
+		// 				}
+		// 			}
+		// 		});
+		// 	});	
+		// }else{
 			heroArray.forEach(arrayCreature =>{
 				var targeted = false;
 				skillList.data.skill[this.identifier[1]].target.forEach((skillTarget, skillTargetIndex)=> {
@@ -4844,7 +4848,7 @@ function onSkillDown(){
 					}
 				});
 			});
-		}
+		// }
 	}
 	if(self){
 		validSkillObjectArray = [];
