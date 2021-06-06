738b016cc75e55ef9df73efdf55a819934b6c12d
NOMADIC\Alvin <alvin.hc.leung@gmail.com>
2021-06-06 16:12:56 -0400

    Update resize.js

--- js/resize.js
+++ js/resize.js
@@ -451,15 +451,19 @@ function resizeInfo(){
 		text.style.fontSize = skillNameFontSize;
 		if(textIndex < 2){
 			text.x = ((textIndex%3)+1) * app.screen.width/13 + app.screen.width/50;
-		}else if(textIndex == 2){
-			text.x = ((textIndex%3)+1) * app.screen.width/11 + app.screen.width/50;
-		}else if(textIndex%3 != 0){
-			text.x = (textIndex%3) * app.screen.width/13 + app.screen.width/50;
-			text.y = Math.floor(textIndex/3) * app.screen.height/12;
-		}else{
+		}else if(textIndex-3 % 4 == 0){
 			text.x = app.screen.width/15;
-			text.y = Math.floor(textIndex/3) * app.screen.height/12;
+			text.y = Math.floor((textIndex-3)/4) * app.screen.height/12;
 		}
+		// }else if(textIndex == 2){
+		// 	text.x = ((textIndex%3)+1) * app.screen.width/11 + app.screen.width/50;
+		// }else if(textIndex%3 != 0){
+		// 	text.x = (textIndex%3) * app.screen.width/13 + app.screen.width/50;
+		// 	text.y = Math.floor(textIndex/3) * app.screen.height/12;
+		// }else{
+		// 	text.x = app.screen.width/15;
+		// 	text.y = Math.floor(textIndex/3) * app.screen.height/12;
+		// }
 	});
 
 	// creatureInfo.info_stat_increase.forEach((increase, increaseIndex)=>{
