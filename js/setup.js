438460caa49a961dddfac673fcdccb276a8bf440
ahcleung <alvin.hc.leung@gmail.com>
2021-07-12 22:34:39 -0400

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -13,7 +13,7 @@ function setup(){
 	textureAdditionalItem = PIXI.Texture.from('img/additional_item.png');
 	textureAdditionalSkip = PIXI.Texture.from('img/additional_skip.png');
 
-	sound = resources.battle_theme.sound;
+	sound = resources.battle_theme2.sound;
 	sound.loop = true;
 
 	// const turnStyle = new PIXI.TextStyle({
